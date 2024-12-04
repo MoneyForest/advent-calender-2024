3083e7a286c92daaba51c70f5a71e679f31e1134
MoneyForest <kanamorishuhei@gmail.com>
2024-12-05 02:20:31 +0900

    add: datadog with otel sdk

--- internal/infrastructure/datadog.go
+++ internal/infrastructure/datadog.go
@@ -5,15 +5,23 @@ import (
 	"os"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	ddotel "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/opentelemetry"
 	ddtracer "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-type ddTracerProvider struct {
+type ddTracerProviderWithDDOTel struct {
 	provider *ddotel.TracerProvider
 }
 
-func InitDatadog() (*ddTracerProvider, error) {
+type ddTracerProvider struct {
+	provider *sdktrace.TracerProvider
+}
+
+func InitDatadogWithDDOTel() (*ddTracerProviderWithDDOTel, error) {
 	tp := ddotel.NewTracerProvider(
 		ddtracer.WithService(os.Getenv("SERVICE")),
 		ddtracer.WithEnv(os.Getenv("ENV")),
@@ -21,9 +29,35 @@ func InitDatadog() (*ddTracerProvider, error) {
 		ddtracer.WithAgentAddr("localhost:8126"),
 	)
 	otel.SetTracerProvider(tp)
+	return &ddTracerProviderWithDDOTel{provider: tp}, nil
+}
+
+func (p *ddTracerProviderWithDDOTel) Shutdown(ctx context.Context) error {
+	return p.provider.Shutdown()
+}
+
+func InitDatadog() (*ddTracerProvider, error) {
+	exporter, err := otlptracehttp.New(
+		context.Background(),
+		otlptracehttp.WithEndpoint("localhost:4319"),
+		otlptracehttp.WithInsecure(),
+		otlptracehttp.WithURLPath("/v1/traces"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(os.Getenv("SERVICE")),
+			semconv.DeploymentEnvironmentKey.String("local"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
 	return &ddTracerProvider{provider: tp}, nil
 }
 
 func (p *ddTracerProvider) Shutdown(ctx context.Context) error {
-	return p.provider.Shutdown()
+	return p.provider.Shutdown(ctx)
 }
